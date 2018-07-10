7c26d98f6a4897edb20f65381652bd6f6c727cb1
Willem Stolte <willem.stolte@deltares.nl>
2018-07-10 17:54:59 +0200

    commented r code

--- R/development.R
+++ R/development.R
@@ -1,11 +1,9 @@
 
 require(tidyverse)
 
-parametermap <- read_delim("../RWS-WebServices/parameters-en-groepen.csv", delim = ";")
-
-devtools::use_data(parametermap)
-
-
+## run once to add parametermap as rdata file to the package
+# parametermap <- read_delim("../RWS-WebServices/parameters-en-groepen.csv", delim = ";")
+# devtools::use_data(parametermap)
 
 #===check functionality=======================
 
@@ -19,5 +17,6 @@ metadata <- rws_metadata() # gets complete catalog
 parsedmetadata <- jsonlite::fromJSON(content(metadata$resp, "text"), simplifyVector = T )
 
 
-parameters <- DDLgetParametersForLocations(parsedmetadata, c("Dreischor", "Herkingen", "Scharendijke diepe put"))
+catalogue <- DDLgetParametersForLocations(parsedmetadata, c("Dreischor", "Herkingen", "Scharendijke diepe put"))
+
 
