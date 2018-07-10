02d445832ec15826e10308bc596ba0b37337cad3
Willem Stolte <willem.stolte@deltares.nl>
2018-07-10 17:58:50 +0200

    r code made as comments

--- R/development.R
+++ R/development.R
@@ -1,22 +1,24 @@
 
-require(tidyverse)
-
-## run once to add parametermap as rdata file to the package
-# parametermap <- read_delim("../RWS-WebServices/parameters-en-groepen.csv", delim = ";")
-# devtools::use_data(parametermap)
-
-#===check functionality=======================
-
-
-source("R/RWS-API.R")
-require(tidyverse)
-
-metadata <- rws_metadata() # gets complete catalog
-
-
-parsedmetadata <- jsonlite::fromJSON(content(metadata$resp, "text"), simplifyVector = T )
-
-
-catalogue <- DDLgetParametersForLocations(parsedmetadata, c("Dreischor", "Herkingen", "Scharendijke diepe put"))
-
-
+# not run
+#
+# require(tidyverse)
+#
+# ## run once to add parametermap as rdata file to the package
+# # parametermap <- read_delim("../RWS-WebServices/parameters-en-groepen.csv", delim = ";")
+# # devtools::use_data(parametermap)
+#
+# #===check functionality=======================
+#
+#
+# source("R/RWS-API.R")
+# require(tidyverse)
+#
+# metadata <- rws_metadata() # gets complete catalog
+#
+#
+# parsedmetadata <- jsonlite::fromJSON(content(metadata$resp, "text"), simplifyVector = T )
+#
+#
+# catalogue <- DDLgetParametersForLocations(parsedmetadata, c("Dreischor", "Herkingen", "Scharendijke diepe put"))
+#
+#
